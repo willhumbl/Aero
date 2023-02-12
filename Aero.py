0d994f0603af2e620c67bd672a10022349b5271f
Will Humble <45468995+willhumbl@users.noreply.github.com>
2023-02-12 15:09:02 -0500

    Add files via upload

---
+++ Aero.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import matplotlib.pyplot as plt
+from markdown2 import Markdown
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def markdown_editor():
+    if request.method == "POST":
+        text = request.form["text"]
+        markdown = Markdown()
+        html = markdown.convert(text)
+        return render_template("markdown_editor.html", text=html)
+    return render_template("markdown_editor.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
