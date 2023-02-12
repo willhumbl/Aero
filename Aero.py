0fb0281383e1abe6a4dbd5f3b86467d5c6cffe76
Will Humble <45468995+willhumbl@users.noreply.github.com>
2023-02-12 16:54:22 -0500

    Update Aero.py

--- Aero.py
+++ Aero.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/app", methods=["GET", "POST"])
 def markdown_editor():
     if request.method == "POST":
         text = request.form["text"]
